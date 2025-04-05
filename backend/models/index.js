45b308d5d7fceaeaad4a42620535379225ada06c
vikasreddy1406 <vikassai345@gmail.com>
2025-04-05 23:36:42 +0530

    added docker file

--- backend/models/index.js
+++ backend/models/index.js
@@ -10,7 +10,7 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbHost = process.env.DB_HOST;
 const dbPort = process.env.DB_PORT;
-const dbDatabase = process.env.DB_DATABASE;
+const dbDatabase = process.env.DB_NAME;
 
 console.assert(dbUser);
 console.assert(dbPassword);
